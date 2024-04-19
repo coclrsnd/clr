457e3c29fa0b66fbc06595e36fc5eeb8ca5118af
Jagadish Veeravalli <jveeravalli@standardrail.com>
2024-04-19 21:10:49 +0530

    Loan Schema Changes Migrations

---
+++ src/Services/User/User.Infrastructure/Migrations/20240419154025_Loan_Scema_changes.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace User.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Loan_Scema_changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OrganizationName",
+                table: "Loans",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrganizationName",
+                table: "Loans");
+        }
+    }
+}
