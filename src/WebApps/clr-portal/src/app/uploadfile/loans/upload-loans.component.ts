d77a483a7827b2301c750a6dccb6e008be950f3e
sattiswathi60@gmail.com <122282323+swathi60@users.noreply.github.com>
2024-04-21 21:23:03 +0530

    reset styles modified

--- src/WebApps/clr-portal/src/app/uploadfile/loans/upload-loans.component.ts
+++ src/WebApps/clr-portal/src/app/uploadfile/loans/upload-loans.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from "@angular/forms";
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { Observable } from "rxjs";
 import { Organization } from "../../sign-up/model/organization";
 import { SignupComponentStore } from "../../sign-up/signup.component.store";
@@ -12,32 +7,40 @@ import { startWith, tap } from "rxjs/operators";
 import { UploadFileRequest } from "../models/upload";
 import { FileUploadService } from "../service/upload.service";
 import { UploadStore } from "./upload-loans.store";
+import { HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: "upload-loans",
-  templateUrl: "upload-loans.component.html",
-  styleUrl: "upload-loans.component.css",
+  templateUrl: "./upload-loans.component.html",
+  styleUrls: ["./upload-loans.component.css"],
 })
 export class UploadLoansComponent implements OnInit {
   filteredOrganizations$: Observable<Organization[]>;
-  selectedFile: any;
+  selectedFile: File | undefined;
+  progressValue: number = 0;
+  progressBarVisible: boolean = false;
+  progressmessage: string = "";
+  progressmsgcolor: string = "black";
+
   organization = new FormControl("", [Validators.required]);
-  uploadStatus$: Observable<boolean> = undefined;
+  uploadStatus$: Observable<boolean> | undefined;
+
   constructor(
     private signupStore: SignupComponentStore,
     private uploadStore: UploadStore,
-  ) {
-    this.uploadStatus$ = this.uploadStore.uploadStatus$;
-  }
+    private uploadService: FileUploadService
+  ) {}
 
   ngOnInit() {
     this.filteredOrganizations$ = this.signupStore.filteredOrganizations$;
     this.organization.valueChanges
       .pipe(
         startWith(""),
-        tap((value) => this.signupStore.filterOrganizations(value)),
+        tap((value) => this.signupStore.filterOrganizations(value))
       )
       .subscribe();
+
+    this.uploadStatus$ = this.uploadStore.uploadStatus$;
   }
 
   displayOrganizationFn(organization?: any): string | undefined {
@@ -46,14 +49,50 @@ export class UploadLoansComponent implements OnInit {
 
   onSubmit() {
     console.log(this.organization.value);
+    this.progressBarVisible = true;
+    this.progressValue = 0;
+
+    if (!this.selectedFile || !this.organization.value['code']) {
+      console.error('File or Organization code is missing');
+      return;
+    }
+
     let uploadFileRequest: UploadFileRequest = {
       file: this.selectedFile,
-      OrgCode: this.organization.value["code"],
+      OrgCode: this.organization.value['code'],
     };
-    this.uploadStore.upload(uploadFileRequest);
+
+    this.uploadStore.upload(uploadFileRequest)
+    // .subscribe(
+    //   (res) => {
+    //     console.log(res);
+    //     if (res.type === HttpEventType.UploadProgress) {
+    //       this.progressValue = Math.round((100 * res.loaded) / res.total);
+    //     } else if (res.type === HttpEventType.Response) {
+    //       console.log(res.body);
+    //     }
+    //   },
+    //   (err) => {
+    //     console.log(err);
+    //     this.progressmsgcolor = "red";
+    //     this.progressmessage = "Upload failed";
+    //     setTimeout(() => {
+    //       this.progressBarVisible = false;
+    //     }, 3000);
+    //   },
+    //   () => {
+    //     this.progressValue = 100;
+    //     this.progressmsgcolor = "green";
+    //     this.progressmessage = "Upload success!";
+    //     setTimeout(() => {
+    //       this.progressBarVisible = false;
+    //     }, 3000);
+    //   }
+    // );
   }
 
-  onFileSelected($event: any) {
-    this.selectedFile = $event.target.files[0];
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+    this.progressmessage = "";
   }
 }
