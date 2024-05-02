3ded00a9c793244a170e87beee8476dda44b8582
swathi60 <sattiswathi60@gmail.com>
2024-05-02 20:30:23 +0530

    toggle bar issue fixed and validated login mail

--- src/WebApps/clr-portal/src/app/auth/login/login.component.ts
+++ src/WebApps/clr-portal/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import {
+  AbstractControl,
   UntypedFormBuilder,
   UntypedFormGroup,
   Validators,
@@ -32,7 +33,7 @@ export class LoginComponent implements OnInit {
     private store: Store<AppState>,
   ) {
     this.form = fb.group({
-      email: ["", [Validators.required, Validators.email]],
+      email: ["", [Validators.required, Validators.email,customEmailValidator]],
       password: ["", [Validators.required]],
     });
   }
@@ -45,4 +46,9 @@ export class LoginComponent implements OnInit {
     const val = this.form.value;
     this.store.dispatch(login({ userName: val.email, password: val.password }));
   }
+}
+export function customEmailValidator(control: AbstractControl): { [key: string]: any } | null {
+  const emailPattern = /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)*(\.[a-zA-Z]{2,})$/;
+  const valid = emailPattern.test(control.value);
+  return valid ? null : { invalidEmail: { value: control.value } };
 }
